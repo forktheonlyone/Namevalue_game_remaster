89f7fa2be71c40b760e53b9b1f4ea98671d6617e
forktheonlyone <forktheonlyone@gmail.com>
2023-09-14 16:20:19 +0900

    여관 클래스 수정 Indev 1.0.3
    - Scene 상속 적용
    - Bill 메서드 수정

--- GoPub.java
+++ GoPub.java
@@ -1,8 +1,8 @@
 // 작성자 : 송한올
 // 2023.09.14
-// indev 1.02
+// indev 1.0.3
 
-public class GoPub {
+public class GoPub extends Scene{
 
     private static GoPub instance = null;
     public static GoPub getInstance() {
@@ -42,21 +42,7 @@ public class GoPub {
     }
 
 
-    public void Bill(Charactor chr, int pay, int heal, String healinfo)
-    {
-        if (((Player)chr).getGold() >= pay)
-        {
-            ((Player)chr).setGold(((Player)chr).getGold() - pay);
-            ((Player)chr).hpRecovery(heal);
-            System.out.println(healinfo);
-        }
-        else
-        {
-            System.out.println("===============");
-            System.out.println("골드가 부족합니다.");
-            System.out.println("===============");
-        }
-    }
+
 
 
     private void PubSelectMenu()
@@ -71,4 +57,62 @@ public class GoPub {
 
     }
 
+    @Override
+    public void Menu() {
+        System.out.print("\n");
+        System.out.println("1. 음식 주문하기 ( 음식을 이용해 체력을 회복하세요. )");
+        System.out.println("2. 방을 빌려 침대에서 잠에 들기 ( -15 Gold )");
+        System.out.println("3. 마을로 돌아가기");
+        System.out.print("\n");
+        return;
+    }
+
+    @Override
+    public Scene Choose() {
+        switch (ScannerManager.Scan())
+        {
+            case 1 :
+                Restaurant restaurant = new Restaurant();
+                restaurant.InRestaurant(Player.getPlayer());
+                GoPub.getInstance().PubMenu(Player.getPlayer());
+
+            case 2 :
+                System.out.println("낡은 침대이지만 포근함 만큼은 집을 생각나게 한다.");
+                System.out.print("\n");
+                Bill(15, 30, "체력을 모두 회복했다!");
+                break;
+
+            case 3 :
+                System.out.println("마을로 돌아간다.");
+                break;
+
+            default:
+                System.out.println("잘못 입력하셨습니다.");
+                break;
+        }
+        return null;
+    }
+
+    @Override
+    public Scene FirstChoose() {
+        System.out.print("\n");
+        System.out.println("맥주잔이 부딪히는 소리, 바드의 악기 연주 소리가 들린다.");
+        return null;
+    }
+    public void Bill(int pay, int heal, String healinfo)
+    {
+        if (Player.getPlayer().getGold() >= pay)
+        {
+            Player.getPlayer().setGold(Player.getPlayer().getGold() - pay);
+            Player.getPlayer().hpRecovery(heal);
+            System.out.println(healinfo);
+        }
+        else
+        {
+            System.out.println("===============");
+            System.out.println("골드가 부족합니다.");
+            System.out.println("===============");
+        }
+    }
 }
+
