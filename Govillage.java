ef90c1f94c835af99a47cd0f51dfdac6d5c8bcd9
stulss <doffl9737@gmail.com>
2023-09-15 12:32:59 +0900

    Update Govillage.java

--- Govillage.java
+++ Govillage.java
@@ -20,7 +20,7 @@ public class Govillage extends Scene {
 
     @Override
     public void Menu() {
-        System.out.println("마을 사람들이 부지런히 움직이고 있다.");
+
         System.out.println("1.여관으로 이동");
         System.out.println("2.대장간으로 이동");
         System.out.println("3.던전으로 이동");
@@ -28,57 +28,33 @@ public class Govillage extends Scene {
     }
 
     @Override
-    public void initialize() {
-
-    }
-
-    @Override
-    public boolean update() {
-        Menu();
-        int id = ScannerManager.Scan();
-        SceneManager.getInstance().setScen(id);
-
-        if(this.Live){
-            return true;}
-        return false;
-    }
-
-    @Override
     public Scene Choose() {
-        return null;
+        Menu();
+        switch(ScannerManager.Scan()){
+            case 1:
+                GoPub.getInstance().FirstChoose();
+                break;
+            case 2:
+                GoSmith.getInstance().FirstChoose();
+                break;
+            case 3:
+                GoDungeon.getInstance().FirstChoose();
+                break;
+            default:
+                System.out.println("잘못 입력 하였습니다.");
+                break;
+
+        }
+        return Choose();
     }
 
     @Override
     public Scene FirstChoose() {
-        return null;
+        Player.getPlayer().Status();
+
+        System.out.println("마을 사람들이 부지런히 움직이고 있다.");
+        System.out.print("\n");
+
+        return Choose();
     }
 }
-
-/*
-    public void ActMenu(Charactor charactor,GoPub goPub,GoSmith goSmith,GoDungeon goDungeon) {
-
-        while (true) {
-            ((Player)charactor).Status();
-            System.out.print("\n");
-            System.out.println("마을 사람들이 부지런히 움직이고 있다.");
-            System.out.print("\n");
-
-
-            switch (ScannerManager.Scan()) {
-                case 1:
-                    goPub.PubMenu(charactor);
-                    break;
-                case 2:
-                    goSmith.SmithMenu(charactor);
-                    break;
-                case 3:
-                    goDungeon.Update(charactor);
-                    break;
-                default:
-                    System.out.println("잘못된 선택지 입니다.");
-                    System.out.println("다시 선택해주세요.");
-                    break;
-            }
-        }
-    }
-*/
