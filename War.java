2b5c1cb688a9b71d9c3a7ae6cf3c2ad13ff04dd3
sejinpark2 <wlstp2367@gmail.com>
2023-09-15 11:41:52 +0900

    워 클래스 파일 업데이트
    내용 수정

--- War.java
+++ War.java
@@ -1,41 +1,140 @@
 // 박세진 , 2023.09.11
 
-public class War {
-    public void Fight(Charactor c, int i){
+import java.sql.SQLOutput;
 
-        switch (i) {
-            case 1 :
-                System.out.println("고블린 소굴에 입장하였습니다.");
-                MonsterManager.getInstance().GoblinList();
-                break;
-            case 2 :
-                System.out.println("오크의 전초기지에 입장하였습니다.");
-                MonsterManager.getInstance().OrkList();
-                break;
-            case 3 :
-                System.out.println("나이트 엘프의 숲에 입장하였습니다.");
-                MonsterManager.getInstance().ElfList();
-                break;
-            case 4 :
-                System.out.println("얼어붙은 설산에 입장하였습니다.");
-                System.out.println("검이 빛나며 빛이 온몸을 감싼다...");
-                System.out.println("얼어붙은 설산의 동굴 속 리치왕 아서스 메네실이 서리한을 집어든다.");
-                MonsterManager.getInstance().LastBoss();
-                ((Player)c).setMaxHp(80);
-                ((Player)c).hpRecovery(80);
-                break;
+public class War extends Scene {
+    private static Scene instance = null;
+
+    public static Scene getInstance() {
+        if (instance == null) {
+            instance = new War();
+        }
+        return instance;
+    }
+
+    public Scene Choose() {
+        Menu();
+        switch (ScannerManager.Scan()) {
+            case 1:
+                AttackSquence(Player.getInstance(),Monster.getInstance());
+                return Dead();
+            case 2:
+                Deffence(Player.getInstance(),Monster.getInstance());
+                return Dead();
+            case 3:
+                NoRun();
+            default:
+                System.out.println("잘못 입력하였습니다.");
+                return Choose();
+        }
+    }
+
+    public void AttackSquence(Charactor player, Charactor enemy) {
+        if (player.getSpeed() > enemy.getSpeed()) {
+            Attack(player, enemy);
+            if (enemy.getHp() > 0)
+                Attack(enemy, player);
+        }
+        else {
+            Attack(enemy, player);
+            if (player.getHp() > 0)
+                Attack(player, enemy);
+        }
+    }
+
+
+    public void Attack(Charactor player, Charactor enemy) {
+        int dmg = player.getAtk() - enemy.getDef();
+        if (dmg < 1) {
+            dmg = 1;
+        }
+        if (enemy.getHp() > dmg) {
+            enemy.setHp(enemy.getHp() - dmg);
+        }
+        else{
+                enemy.setHp(0);
+            }
+            System.out.println(player.getNickName() + "(이)가 " + enemy.getNickName() + "에게 "
+                    + dmg + "만큼 데미지를 주었습니다. ");
+            System.out.println(enemy.getNickName() + "의 현재 HP : " + enemy.getHp());
+
         }
 
+    public void Deffence(Charactor player, Charactor enemy){
+        int dmg = (enemy.atk - player.def + 1) / 2;
+        if (dmg < 1) {
+            dmg = 1;
+        }
+        System.out.println(enemy.getNickName() + "(이)가 " + player.getNickName() + "에게 "
+                + dmg + "만큼 데미지를 주었습니다. ");
 
-        boolean keep =true;
+    }
+
+    public Scene Dead() {
+        if (Player.getInstance().getHp() < 1) {
+            System.out.println(Player.getInstance().getNickName() + "의 현재 HP : 0");
+            System.out.println(Player.getInstance().getNickName() + "(은)는 픽 쓰러졌다.\n\n" +
+                    "더 이상 일어설 힘도 나지 않고 눈은 점점 감긴다.\n" +
+                    "누군가가 도움을 줬으면 했다.\n\n" +
+                    "하지만 아무도 오지 않았다.\n\n" +
+                    "Bad Ending");
+            System.exit(0);
+            return null;
+        } else if (Monster.getInstance().getHp() < 1) {
+            return null;
+        } else {
+            return Choose();
+        }
+    }
+
+    public void Menu() {
+        System.out.println("1.공격하기 ");
+        System.out.println("2.막기 ");
+        System.out.println("3.도망가기 ");
+    }
+    public Scene FirstChoose() {
+        System.out.println(Monster.getInstance().getNickName() + "(이)가 나타났다!");
+        return Choose();
+
+    }
+
+    public Scene NoRun() {
+        if (Monster.getInstance().getNickName().equals("리치왕 아서스 메놀드")) {
+            System.out.println("도망칠 수 없음");
+            return Choose();
+        }
+        else
+            return Govillage.getInstance().FirstChoose();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       /*
+
+        boolean keep = true;
 
         while (MonsterManager.getInstance().size() > 0) {
             Monster m = MonsterManager.getInstance().getMonsterList(0);
-            if(keep) {
-                System.out.println(m.getNickName() + "(이)가 나타났다!");
-            }
+
+            System.out.println(m.getNickName() + "(이)가 나타났다!");
+
             while (m.getHp() > 0 && keep) {
                 PlayActMenu();
+
                 switch (ScannerManager.Scan()) {
                     case 1:
                         if (c.getSpeed() > m.getSpeed()) {
@@ -61,26 +160,26 @@ public class War {
                         break;
                 }
             }
-            if(keep) {
-                System.out.println(m.getNickName() + "(이)가 죽었습니다.");
-            }
+
+            System.out.println(m.getNickName() + "(이)가 죽었습니다.");
+
             MonsterManager.getInstance().remove();
         }
 
-        if(keep) {
-            switch (i) {
-                case 1 :
-                    GetGold(c,30);
-                    break;
-                case 2 :
-                    GetGold(c,45);
-                    break;
-                case 3 :
-                    GetGold(c,60);
-                    break;
-            }
+
+        switch (i) {
+            case 1 :
+                GetGold(c,30);
+                break;
+            case 2 :
+                GetGold(c,45);
+                break;
+            case 3 :
+                GetGold(c,60);
+                break;
         }
 
+        MonsterManager.getInstance().getMonsterList(1);
     }
 
     public void PlayActMenu() {
@@ -93,4 +192,6 @@ public class War {
         System.out.println(gold + "골드를 획득하였습니다.");
         ((Player)c).setGold(((Player)c).getGold()+gold);
     }
-}
+
+    */
+
