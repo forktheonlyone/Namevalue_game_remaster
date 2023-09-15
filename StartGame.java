3eaf1246ed5dde591d49ed3d9d86bd276563d3a8
stulss <doffl9737@gmail.com>
2023-09-15 18:01:06 +0900

    Update StartGame.java

--- StartGame.java
+++ StartGame.java
@@ -6,14 +6,37 @@ public class StartGame {
     public static void main(String[] args) {
 
         ScannerManager.Initialize();
-        DialogManager.getInstance().TitleDialog();
+        System.out.println
+                (
+                                "   ■     ■  ■■■■■   ■     ■     ■  \n" +
+                                "   ■     ■      ■   ■  ■■■■■■■  ■  \n" +
+                                "   ■     ■      ■   ■    ■■■    ■  \n" +
+                                "   ■     ■      ■   ■   ■■■■■   ■  \n" +
+                                "   ■■ ■■■■  ■■■■■   ■  ■■   ■■  ■■■\n" +
+                                "  ■■■    ■  ■       ■  ■■   ■■  ■  \n" +
+                                " ■■■■■   ■  ■       ■   ■■■■■   ■  \n" +
+                                "■■■  ■■  ■  ■       ■    ■■■    ■  \n" +
+                                "■■    ■  ■  ■■■■■■■ ■    ■      ■  \n" +
+                                "         ■          ■    ■■■■■■■■  \n" +
+                                "\n NameValue's Text RPG \n "+
+                                "'Frostmourne Sword'\n");
+
+        System.out.println("1. 게임 시작");
+        System.out.println("2. 종료");
+
         switch (ScannerManager.Scan())
         {
             case 1 :
-                Govillage.getInstance().FirstChoose();
+                SceneManager.getInstance().getVillage().FirstChoose();
                 break;
             case 2:
                 break;
         }
+
+
+
+
+
+
     }
 }
