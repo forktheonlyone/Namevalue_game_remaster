28d3b1f24fe73c743b485bbe62bdc8d853d49797
forktheonlyone <forktheonlyone@gmail.com>
2023-09-18 10:25:13 +0900

    여관, 대장간, 식당 업데이트
    - 여관
    * 씬매니저 식당 적용
    - 식당
    * 씬매니저 식당 오버라이드 적용
    - 대장간
    * 다이얼로그매니저 적용

--- GoSmith.java
+++ GoSmith.java
@@ -4,40 +4,25 @@
 
 public class GoSmith extends Scene {
 
-    private int[][] gear =
+    public int[][] gear =
             {
                     {4, 1},
                     {6, 2},
                     {8, 3}
             };
-    private int[][] upgradesGold =
+    public int[][] upgradesGold =
             {
                     {50, 30},
                     {100, 60},
                     {150, 90}
             };
 
-    private int swordUpLv = 0;
-    private int armorUpLv = 0;
+    public int swordUpLv = 0;
+    public int armorUpLv = 0;
 
     @Override
     public void Menu() {
-        if (swordUpLv == 3)
-        {
-            System.out.println("무기 강화 최대치");
-        }
-        else
-        {
-            System.out.println("1. 무기 강화 ( " + upgradesGold[swordUpLv][0] + " )");
-        }
-        if (armorUpLv == 3)
-        {
-            System.out.println("방어구 강화 최대치");
-        }
-        else
-        {
-            System.out.println("2. 방어구 강화 ( " + upgradesGold[armorUpLv][1] + " )");
-        }
+        DialogManager.getInstance().SmithUpgradeMenu();
         DialogManager.getInstance().GoBackTownDialog();
     }
     @Override
