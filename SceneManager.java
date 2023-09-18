a7f7593685ce4539f8d6a4fd9d564edcaa507b22
stulss <doffl9737@gmail.com>
2023-09-18 11:54:40 +0900

    Update SceneManager.java

--- SceneManager.java
+++ SceneManager.java
@@ -16,43 +16,48 @@ public class SceneManager {
     private final Scene smith = new GoSmith();
     private final Scene dungeon = new GoDungeon();
     private final Scene war = new War();
+    private final Scene restaurant = new Restaurant();
     private final Dungeon D = new Dungeon();
 
     public GoVillage getVillage() {return (GoVillage)village;}
     public GoPub getPub() {return (GoPub)pub;}
     public GoSmith getSmith() {return (GoSmith)smith;}
     public GoDungeon getDungeon() {return (GoDungeon)dungeon;}
+    public Restaurant getRestaurant() {return (Restaurant)restaurant;}
     public War getWar() {return (War)war;}
+    public Dungeon getD() {return D;}
 
-    public Scene sceneSelect() {
-        int id = ScannerManager.Scan();
-        switch (id) {
+    public void VillageMoveSelect() {
+        switch (ScannerManager.Scan()) {
             case 1:
-                return SceneManager.getInstance().getPub().FirstChoose();
+                getPub().FirstChoose();
+                return;
             case 2:
-                return SceneManager.getInstance().getSmith().FirstChoose();
+                getSmith().FirstChoose();
+                return;
             case 3:
-                return SceneManager.getInstance().getDungeon().FirstChoose();
+                getDungeon().FirstChoose();
+                return;
             default:
                 System.out.println("잘못 입력 하였습니다.");
                 break;
         }
-        return getVillage().Choose();
+        getVillage().Choose();
     }
 
-    public Scene DungeonSelect() {
+    public void DungeonSelect() {
         switch (ScannerManager.Scan()) {
             case 1:
-                D.GoblinD();
+                getD().GoblinD();
                 break;
             case 2:
-                D.OrkD();
+                getD().OrkD();
                 break;
             case 3:
-                D.ElfD();
+                getD().ElfD();
                 break;
             case 4:
-                D.LichD();
+                getD().LichD();
                 DialogManager.getInstance().TrueEnding();
                 System.exit(0);
                 break;
@@ -62,6 +67,6 @@ public class SceneManager {
                 DialogManager.getInstance().WrongDialog();
                 break;
         }
-        return SceneManager.getInstance().getVillage().FirstChoose();
+        getVillage().FirstChoose();
     }
 }
