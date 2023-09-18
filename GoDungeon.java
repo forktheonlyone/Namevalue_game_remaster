2035792ffcf89437582e43afd72620111c0254ac
sejinpark2 <wlstp2367@gmail.com>
2023-09-18 11:21:29 +0900

    고던전 클래스 파일 수정
    switch문 씬매니저로 옮김

--- GoDungeon.java
+++ GoDungeon.java
@@ -2,52 +2,18 @@
 
 public class GoDungeon extends Scene {
 
-    private static GoDungeon instance = null;
-    public static GoDungeon getInstance() {
-        if (instance == null) {
-            instance = new GoDungeon();
-        }
-        return instance;
-    }
-
-    Dungeon D = new Dungeon();
-
     public void Menu() {
         DialogManager.getInstance().DungeonMenuDialog();
     }
 
     public Scene Choose() {
         Menu();
-        setDungeon(new Dungeon());
-        switch (ScannerManager.Scan()) {
-            case 1:
-                D.GoblinD();
-                break;
-            case 2:
-                D.OrkD();
-                break;
-            case 3:
-                D.ElfD();
-                break;
-            case 4:
-                D.LichD();
-                DialogManager.getInstance().TrueEnding();
-                System.exit(0);
-                break;
-            case 5:
-                break;
-            default:
-                DialogManager.getInstance().WrongDialog();
-                break;
-        }
-         return SceneManager.getInstance().getVillage().FirstChoose();
+        SceneManager.getInstance().DungeonSelect();
+        return null;
     }
 
     public Scene FirstChoose() {
         return Choose();
     }
 
-    public void setDungeon(Dungeon D){
-        this.D = D;
-    }
 }
